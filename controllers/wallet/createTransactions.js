5571cb1e4c7ad06a520211f80018f1aaf7dd4145
koval-andrey <70697160+koval-andrey@users.noreply.github.com>
2021-08-22 13:55:28 +0300

    wallet
    исправления в creatTransactions

--- controllers/wallet/createTransactions.js
+++ controllers/wallet/createTransactions.js
@@ -28,7 +28,7 @@ const ALLOWED_CATEGORIES = {
     OTHER_EXPENSES,
   ],
 }
-const createTransaction = async (req, re, next) => {
+const createTransaction = async (req, res, next) => {
   const { date, type, category, comments, amount, id } = req.body
   const userId = req.userId._id
 
@@ -45,41 +45,41 @@ const createTransaction = async (req, re, next) => {
         message: 'Access to the target resource was denied'
       })
     }
-      if (type === COST && amountNumber > wallet.total) {
-        return res.status(412).json({
-          status: 'error',
-          code: 412,
-          message: 'Insufficient funds for this operation'
-        })
+    if (type === COST && amountNumber > wallet.total) {
+      return res.status(412).json({
+        status: 'error',
+        code: 412,
+        message: 'Insufficient funds for this operation'
+      })
     }
-        const balanceAfter = type === COST
-          ? +wallet.total - amountNumber
-          : +wallet.total + amountNumber
+    const balanceAfter = type === COST
+      ? +wallet.total - amountNumber
+      : +wallet.total + amountNumber
 
-        const transformType = type === COST ? '-' : '+'
-        const transaction = new Transaction({
-          id,
-          date,
-          type: transformType,
-          category,
-          comments,
-          amount: amountNumber,
-          balanceAfter,
-        })
-        wallet.total = balanceAfter
-        if (type === COST) {
-          wallet.costs += amountNumber;
-        }
-        if (type === INCOME) {
-          wallet.income += amountNumber;
-        }
-        const savedTransaction = await transaction.save();
-        wallet.transactions = [...wallet.transactions, savedTransaction._id];
-        await wallet.save();
-        res.json(savedTransaction);
-      }
-    catch (error) {
+    const transformType = type === COST ? '-' : '+'
+    const transaction = new Transaction({
+      id,
+      date,
+      type: transformType,
+      category,
+      comments,
+      amount: amountNumber,
+      balanceAfter,
+    })
+    wallet.total = balanceAfter
+    if (type === COST) {
+      wallet.costs += amountNumber;
+    }
+    if (type === INCOME) {
+      wallet.income += amountNumber;
+    }
+    const savedTransaction = await transaction.save();
+    wallet.transactions = [...wallet.transactions, savedTransaction._id];
+    await wallet.save();
+    res.json(savedTransaction);
+  } catch (error) {
     next(error)
   }
+}
 
-module.exports = { createTransaction }
+module.exports = createTransaction
