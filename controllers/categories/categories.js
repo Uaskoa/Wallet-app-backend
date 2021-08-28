65c91ac199e268dce3f25c213fc0eb7a6ae55f22
koval-andrey <kovalandrey1234@gmail.com>
2021-08-28 16:07:25 +0300

    wallet
    1

--- controllers/categories/categories.js
+++ controllers/categories/categories.js
@@ -1,13 +1,13 @@
 const service = require('../../services/categories')
 
 const getAllCategories = async (req, res, next) => {
-    try {
-        const result = await service.getAllCategories()
-        return res.json({
-            data:{result}
-        })
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const result = await service.getAllCategories()
+    return res.json({
+      data: { result }
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 module.exports = getAllCategories
--- controllers/wallet/createTransactions.js
+++ controllers/wallet/createTransactions.js
@@ -31,22 +31,18 @@ const ALLOWED_CATEGORIES = {
   ],
 }
 
-
 const createTransaction = async (req, res, next) => {
   const { date, type, category, comments, amount } = req.body
-  const year = date.substring(0,4)
-  const month = date.substring(5,7)
+  const year = date.substring(0, 4)
+  const month = date.substring(5, 7)
 
   const userId = req.user._id
   try {
     let balanceAfter
     const amountNumber = +amount
 
-
     const resultBalance = (lastBalance) => {
-      
-      return type === COST ? lastBalance - amountNumber:lastBalance + amountNumber
-      
+      return type === COST ? lastBalance - amountNumber : lastBalance + amountNumber
     }
 
     const lastBalance = await TA.find({ createdBy: userId }).sort({ $natural: -1 }).limit(1)
@@ -54,42 +50,40 @@ const createTransaction = async (req, res, next) => {
     if (lastBalance[0] === undefined) {
       if (type === COST) {
         return res.json({
-        'messege':'Баланс не может быть отрицательным'
-      })
+          messege: 'Баланс не может быть отрицательным'
+        })
       }
-     balanceAfter = resultBalance(0)
+      balanceAfter = resultBalance(0)
     } else {
-       if (lastBalance[0].balanceAfter < amountNumber && type === COST) {
-      return res.json({
-        'messege':'Баланс не может быть отрицательным'
-      })
+      if (lastBalance[0].balanceAfter < amountNumber && type === COST) {
+        return res.json({
+          messege: 'Баланс не может быть отрицательным'
+        })
+      }
+      balanceAfter = resultBalance(lastBalance[0].balanceAfter)
     }
-     balanceAfter = resultBalance(lastBalance[0].balanceAfter)
-    }
-    
-    const result = await service.add({date, type, category, comments, amount,year,month,balanceAfter,createdBy:req.user._id, userId: req.user._id,})
+
+    const result = await service.add({ date, type, category, comments, amount, year, month, balanceAfter, createdBy: req.user._id, userId: req.user._id, })
     return res.json({
-     data:{result}
-   })
-  
-    
-    
+      data: { result }
+    })
+
     // const wallet = await Wallet.findById(user.wallet._id)
     // console.log(wallet);
-  //   if (!ALLOWED_CATEGORIES[type].includes(category)) {
-  //     return res.status(412).json({
-  //       status: 'error',
-  //       code: 412,
-  //       message: 'Access to the target resource was denied'
-  //     })
-  //   }
-  //   if (type === COST && amountNumber > wallet.total) {
-  //     return res.status(412).json({
-  //       status: 'error',
-  //       code: 412,
-  //       message: 'Insufficient funds for this operation'
-  //     })
-  //   }
+    //   if (!ALLOWED_CATEGORIES[type].includes(category)) {
+    //     return res.status(412).json({
+    //       status: 'error',
+    //       code: 412,
+    //       message: 'Access to the target resource was denied'
+    //     })
+    //   }
+    //   if (type === COST && amountNumber > wallet.total) {
+    //     return res.status(412).json({
+    //       status: 'error',
+    //       code: 412,
+    //       message: 'Insufficient funds for this operation'
+    //     })
+    //   }
     // const balanceAfter = type === COST
     //   ? +wallet.total - amountNumber
     //   : +wallet.total + amountNumber
